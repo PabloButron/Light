6747b804b5243672a9200edb9caf2ca60efd82cf
Pablo Butron <lizardlabs@icloud.com>
2022-11-03 12:48:45 -0400

    at the beginning there was darkness

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
+var lightOn = true
+
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+  
+    fileprivate func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+      
+        updateUI()
 
+        
+    }
+    
+
+    
 }
 

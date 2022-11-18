8e5844a9f4000bb5ca6314a39b3bca6a2aded3ef
Pablo Butron <lizardlabs@icloud.com>
2022-11-18 16:19:50 -0400

    Added Comments of classes

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -11,15 +11,19 @@ var lightOn = true
 
 class ViewController: UIViewController {
 
+    // ViewController is a child class of UIViewController has inheritance
     
     override func viewDidLoad() {
+        // Here we override the func viewDidLoad of the parent class
         super.viewDidLoad()
+        //Here we call the super class and the method viewDidLoad
         // Do any additional setup after loading the view.
     }
 
   
     fileprivate func updateUI() {
         view.backgroundColor = lightOn ? .white : .black
+        // We are modifying the parent class UIViewController
     }
     
     @IBAction func buttonPressed(_ sender: Any) {
